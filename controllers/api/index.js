ec155d39de1965496fce41d43fc83601f573a8ac
Mike Ruane <mikeruanedev@gmail.com>
2021-04-07 20:13:17 -0400

    add table parse function, simplify routes for new NutriInfo model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 // API ROUTES
 const userRoutes = require('./userRoutes');
-const ingredientRoutes = require('./ingredient-routes');
+const nutritionInfoRoutes = require('./nutritionInfo-routes');
+//const ingredientRoutes = require('./ingredient-routes');
 
 router.use('/users', userRoutes);
-router.use('/ingredients', ingredientRoutes);
+router.use('/nutritionInfo', nutritionInfoRoutes);
+//router.use('/ingredients', ingredientRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,18 +1,18 @@
 const User = require('./user');
 const NutriInfo = require('./nutritionInfo');
-const Ingredient = require('./ingredient');
+//const Ingredient = require('./ingredient');
 
-User.hasMany(Ingredient, {
-  foreignKey: 'user_id',
-});
-Ingredient.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-Ingredient.hasMany(NutriInfo, {
-  foreignKey: 'food_id',
-});
-NutriInfo.belongsTo(Ingredient, {
-  foreignKey: 'food_id',
-});
+// User.hasMany(Ingredient, {
+//   foreignKey: 'user_id',
+// });
+// Ingredient.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
+// Ingredient.hasMany(NutriInfo, {
+//   foreignKey: 'food_id',
+// });
+// NutriInfo.belongsTo(Ingredient, {
+//   foreignKey: 'food_id',
+// });
 
-module.exports = { User, NutriInfo, Ingredient };
+module.exports = { User, NutriInfo };
